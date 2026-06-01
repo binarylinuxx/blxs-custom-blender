33d3e4d9ac116c73e76a4c150f1094df79a58ad6
binarylinuxx <aar58384@gmail.com>
2026-06-01 08:28:52 +0300

    fix: use writable ccache dir in CI

--- flake.nix
+++ flake.nix
@@ -58,7 +58,7 @@
                 extraConfig = ''
                   export CCACHE_COMPRESS=1
                   export CCACHE_SLOPPINESS=random_seed
-                  export CCACHE_DIR=''${CCACHE_DIR:-/var/cache/ccache}
+                  export CCACHE_DIR=''${CCACHE_DIR:-''${TMPDIR:-/tmp}/ccache}
                   export CCACHE_UMASK=007
                 '';
               };
